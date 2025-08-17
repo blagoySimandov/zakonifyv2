c2f040fbf58509896f34c4de5ff825336218cbc5
blagoySimandov <baggy.simandoff@gmail.com>
2025-08-17 22:00:33 +0300

    feat: clerk integration with whatever this convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+console.log();
+
+const config = {
+  providers: [
+    {
+      domain: process.env.CLERK_FRONTEND_API_URL,
+      applicationID: "convex",
+    },
+  ],
+};
+export default config;
